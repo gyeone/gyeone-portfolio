eb8fe5a9b7e53329f7381ea1b8064d36e46e0061
gyeone <voov000@naver.com>
2026-02-22 20:40:30 +0900

    contact페이지 - 기본 내용 추가와 css적용

---
+++ gyeone/src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import "../styles/Contact.css";
+
+function Contact() {
+    return (
+        <div className="contact">
+            <div className="contact-title">
+                <div className="contact-title-left-division" />
+                <p>Contact</p>
+                <div className="contact-title-right-division" />
+            </div>
+        </div>
+    );
+}
+export default Contact;
