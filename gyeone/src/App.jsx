b2a059c83e8c1c90963ea7fe756a46847cfe1498
gyeone <voov000@naver.com>
2026-02-22 20:32:45 +0900

    fullpage적용, 기본적인 css적용

--- gyeone/src/App.jsx
+++ gyeone/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "./styles/Reset.css";
+import { useState } from "react";
+import {
+    FullpageContainer,
+    FullpageSection,
+} from "@shinyongjun/react-fullpage";
+import "@shinyongjun/react-fullpage/css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Aboutme from "./pages/Aboutme";
+import Skills from "./pages/Skills";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [activeIndex, setActiveIndex] = useState(0);
+    return (
+        <>
+            <Header activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+            <FullpageContainer
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+            >
+                <FullpageSection name="home">
+                    <div>
+                        <Home />
+                    </div>
+                </FullpageSection>
+                <FullpageSection name="aboutme">
+                    <div>
+                        <Aboutme />
+                    </div>
+                </FullpageSection>
+                <FullpageSection name="skills">
+                    <div>
+                        <Skills />
+                    </div>
+                </FullpageSection>
+                <FullpageSection name="project">
+                    <div>
+                        <Project />
+                    </div>
+                </FullpageSection>
+                <FullpageSection name="contact" isAutoHeight>
+                    <div>
+                        <Contact />
+                    </div>
+                </FullpageSection>
+            </FullpageContainer>
+        </>
+    );
 }
 
-export default App
+export default App;
